b1600ac7451bbe5c773bda119c574e95a4a389ac
Adrian Padin <apadin1@users.noreply.github.com>
2016-03-26 18:10:26 -0400

    Update main.c

--- main.c
+++ main.c
@@ -21,11 +21,6 @@ int main() {
 	/********** INITIALIZATION **********/
 	/************************************/
 
-	printf("\r\n\r\nHBridge: %x\r\n", (int) getHBridgeInputs());
-
-	setHBridgeInputs(0);
-
-
 	int i = 0;
 	for (i = 0; i < 1000000; ++i);
 
@@ -60,14 +55,6 @@ int main() {
 	/***************************************/
 
 	MSS_SPI_init( &g_mss_spi1 );
-	MSS_SPI_configure_master_mode
-	(
-		&g_mss_spi1,
-		MSS_SPI_SLAVE_1,
-		MSS_SPI_MODE3,		  // Clock idle high, read on rising edge
-		MSS_SPI_PCLK_DIV_256, // Clock period of 20MHz / 256 ~= 78 KHz
-		MSS_SPI_BLOCK_TRANSFER_FRAME_SIZE
-	);
 
 	// Initialize controllers here
 	controller_t controller1;
@@ -79,10 +66,6 @@ int main() {
 	//controller_init(&controller2, MSS_SPI_SLAVE_2);
 	//controller_init(&controller3, MSS_SPI_SLAVE_3);
 	//controller_init(&controller4, MSS_SPI_SLAVE_4);
-
-	/* Debugging
-	digital_capture(&controller1);
-	*/
 
 	setup_all(&controller1);
 	//setup_all(&controller2);
@@ -106,13 +89,24 @@ int main() {
 
 
 		printf("\r\nResponses %d:\r\n", i);
-		int j; for (j = 0; j < 18; ++j) {
+		int j; for (j = 0; j < 16; ++j) {
 			printf("Buffer[%d]: %d\r\n", j, (unsigned int) flip(controller1.slave_buffer[j]));
 		}
 
 
 
 		/********** SEND DATA TO CARS ********/
+
+		uint8_t tx_buff[2] = { flip(controller1.slave_buffer[1]), flip(controller1.slave_buffer[3]) };
+		tx_buff[1] |= 0x01;
+		tx_buff[0] &= 0xFE;
+		printf("sending: %x, %x", tx_buff[0], tx_buff[1]);
+		MSS_UART_polled_tx(
+			&g_mss_uart1,
+			tx_buff,
+			2 // number of bytes to send
+		);
+
 
 		/*
 		MSS_UART_polled_tx(
@@ -132,27 +126,13 @@ int main() {
 			controller1.slave_buffer + 12,
 			2 // number of bytes to send
 		);
-
-		MSS_UART_polled_tx(
-			&g_mss_uart1,
-			controller1.slave_buffer + 12,
-			2 // number of bytes to send
-		);
-*/
-
-		if (controller1.slave_buffer[12] > 0x10) {
-			setHBridgeInputs(0x1);
-		}
-		else {
-			setHBridgeInputs(0x0);
-		}
-
+		 */
 
 		/********** RECEIVE DATA FROM CARS **********/
 
 		/********** CHANGE VIBRATION DATA **********/
 
-		set_vibration(&controller1, 0xFF, 10);
+		//set_vibration(&controller1, 0xFF, 10);
 		//set_vibration(&controller2, 0xFF, 10);
 		//set_vibration(&controller3, 0xFF, 10);
 		//set_vibration(&controller4, 0xFF, 10);
