fe7215808adc688f58a8b7b75ad1511e3d42e6c0
bmiron <bmiron@umich.edu>
2016-03-28 17:49:59 -0400

    Updated Code
    CHANGES:
    The code now expects 8 bytes to be sent
    The first byte is an identifier (0x01 - 0x04)
    Cleaned up the setHBridgeInputs and setPWMDuty functions

--- main.c
+++ main.c
@@ -1,25 +1,123 @@
 /**************************************\
- * File Name:      main.c
- * Project Name:   EECS373 Final Project
- * Created by:     Adrian Padin
- * Start date:     15 March 2016
- * Last modified:  22 March 2016
+ * File Name:     	main.c
+ * Project Name:   	EECS373 Final Project
+ * Created by:     	Adrian Padin
+ * Edited by:		Ben Miron
+ * 					Emily Rowland
+ * 					Jawad Nasser
+ * Start date:     	15 March 2016
+ * Last modified:  	22 March 2016
 \**************************************/
 
 #include <stdio.h>
 #include <inttypes.h>
-#include "drivers/mss_spi/mss_spi.h"
 #include "drivers/mss_uart/mss_uart.h"
 #include "drivers/mss_gpio/mss_gpio.h"
-#include "ps2.h"
 #include "pwm.h"
 
+/************************************/
+/******** Define and Globals ********/
+/************************************/
+
+//Define for the RX_BUFF for UART1
+#define RX_BUFF_SIZE    2
+
+
+//UART1 buffer for input
+uint8_t g_rx_buff[RX_BUFF_SIZE];
+uint8_t g_rx_idx = 0;
+
+
+/************************************/
+/******** Interrupt Handlers ********/
+/************************************/
+
+
+//UART RX Interrupt Handler
+void uart1_rx_handler( mss_uart_instance_t * this_uart ){
+	int data_received = 0;
+
+	//data_received = the number of bytes that were saved into g_rx_buff
+	//This value should be 2, since we are receiving 2 bytes of data at a time
+    data_received = MSS_UART_get_rx( this_uart, g_rx_buff, sizeof(g_rx_buff) );
+
+
+    //If this value is 0, an interrupt should NOT have been thrown
+    if (data_received == 0){
+    	printf("I'm not sure how this happened, but you probably should check it out.\r\n");
+    }
+
+    //If we received more than 2 bytes, there is a problem with the uart input
+    else if (data_received > 2){
+    	printf("No idea how this happened either. Check it out.\r\n");
+    }
+
+    /*
+	//The data was received at least partially correctly
+    else if(data_received == 1){
+        		printf("received incomplete data, processing anyway\r\n");
+    }
+	*/
+
+    //Otherwise, the data was received correctly
+    else{
+
+    	//If we are trying to control the right wheel with the first byte (LSB is 0)
+		if(!(g_rx_buff[0] & 0x01)){
+
+			setPWMDutyRight(calc_duty(g_rx_buff[0]));
+			setHBridgeInputs(calc_HBridgeRight(g_rx_buff[0]));
+		}
+
+		//Trying to control the left wheel with the first byte (LSB is 1)
+		if(g_rx_buff[0] & 0x01){
+
+			setPWMDutyLeft(calc_duty(g_rx_buff[0]));
+			setHBridgeInputs(calc_HBridgeLeft(g_rx_buff[0]));
+
+		}
+
+		//Trying to control the right wheel with the second byte (LSB is 0)
+		/*
+		if(!(g_rx_buff[1] & 0x01)){
+
+			setPWMDutyRight(calc_duty(g_rx_buff[1]));
+			setHBridgeInputs(calc_HBridgeRight(g_rx_buff[1]));
+		}
+
+		//Trying to control the left wheel with the second byte (LSB is 1)
+		if(g_rx_buff[1] & 0x01){
+
+			setPWMDutyLeft(calc_duty(g_rx_buff[1]));
+			setHBridgeInputs(calc_HBridgeLeft(g_rx_buff[1]));
+
+		}
+		*/
+    }
+    printf("Data received! %x\r\n", g_rx_buff[g_rx_idx]);
+    uint32_t pwmDuty = getPWMDuty();
+    printf("HBridge Inputs: %x | Left Duty: %x | Right duty: %x\r\n", getHBridgeInputs() & 0x0F,
+    		(pwmDuty & PWM_RIGHT_MASK) >> 8, pwmDuty & PWM_LEFT_MASK);
+}
+
+
+
+
+/************************************/
+/*************** MAIN ***************/
+/************************************/
 
 int main() {
 
 	/************************************/
 	/********** INITIALIZATION **********/
 	/************************************/
+
+	// printf("\r\n\r\nHBridge: %x\r\n", (int) getHBridgeInputs());
+	setPWMDutyRight(0);
+	setPWMDutyLeft(0);
+	setHBridgeInputs(0);
+
 
 	int i = 0;
 	for (i = 0; i < 1000000; ++i);
@@ -32,6 +130,15 @@ int main() {
 		&g_mss_uart1,
 		MSS_UART_9600_BAUD,
 		(MSS_UART_DATA_8_BITS | MSS_UART_NO_PARITY | MSS_UART_ONE_STOP_BIT)
+	);
+
+	//MSS_UART_FIFO_EIGHT_BYTES
+	//MSS_UART_FIFO_SINGLE_BYTE
+	MSS_UART_set_rx_handler
+	(
+		     &g_mss_uart1,
+		     uart1_rx_handler,
+		     0x20
 	);
 
 	/********************************/
@@ -50,99 +157,22 @@ int main() {
 	MSS_GPIO_set_output (MSS_GPIO_15,	1);
 	*/
 
-	/***************************************/
-	/********** SETUP CONTROLLERS **********/
-	/***************************************/
-
-	MSS_SPI_init( &g_mss_spi1 );
-
-	// Initialize controllers here
-	controller_t controller1;
-	//controller_t controller2;
-	//controller_t controller3;
-	//controller_t controller4;
-
-	controller_init(&controller1, MSS_SPI_SLAVE_1);
-	//controller_init(&controller2, MSS_SPI_SLAVE_2);
-	//controller_init(&controller3, MSS_SPI_SLAVE_3);
-	//controller_init(&controller4, MSS_SPI_SLAVE_4);
-
-	setup_all(&controller1);
-	//setup_all(&controller2);
-	//setup_all(&controller3);
-	//setup_all(&controller4);
-
 
 	/*******************************/
 	/********** MAIN LOOP **********/
 	/*******************************/
 
-	for (i = 0; 1; ++i) {
+	while(1) {
 
 
-		/********** CAPTURE CONTROLLER DATA ********/
+		//printf("\r\nResponses %d:\r\n", i);
 
-		full_capture(&controller1);
-		//full_capture(&controller2);
-		//full_capture(&controller3);
-		//full_capture(&controller4);
+		/* No code needed here, all in interrupt handlers */
 
-
-		printf("\r\nResponses %d:\r\n", i);
-		int j; for (j = 0; j < 16; ++j) {
-			printf("Buffer[%d]: %d\r\n", j, (unsigned int) flip(controller1.slave_buffer[j]));
-		}
-
-
-
-		/********** SEND DATA TO CARS ********/
-
-		uint8_t tx_buff[2] = { flip(controller1.slave_buffer[1]), flip(controller1.slave_buffer[3]) };
-		tx_buff[1] |= 0x01;
-		tx_buff[0] &= 0xFE;
-		printf("sending: %x, %x", tx_buff[0], tx_buff[1]);
-		MSS_UART_polled_tx(
-			&g_mss_uart1,
-			tx_buff,
-			2 // number of bytes to send
-		);
-
-
-		/*
-		MSS_UART_polled_tx(
-			&g_mss_uart1,
-			controller1.slave_buffer + 12,
-			2 // number of bytes to send
-		);
-
-		MSS_UART_polled_tx(
-			&g_mss_uart1,
-			controller1.slave_buffer + 12,
-			2 // number of bytes to send
-		);
-
-		MSS_UART_polled_tx(
-			&g_mss_uart1,
-			controller1.slave_buffer + 12,
-			2 // number of bytes to send
-		);
-		 */
-
-		/********** RECEIVE DATA FROM CARS **********/
-
-		/********** CHANGE VIBRATION DATA **********/
-
-		//set_vibration(&controller1, 0xFF, 10);
-		//set_vibration(&controller2, 0xFF, 10);
-		//set_vibration(&controller3, 0xFF, 10);
-		//set_vibration(&controller4, 0xFF, 10);
-
-
-
-
-		j = 0; for (j = 0; j < 10000; ++j); // Delay
 	}
 
-	// If you've made it this far, something went wrong
+	// If you've made it this far, something went very very wrong
+	printf("RIP, yer code be ded\r\n");
+
 	return(1);
 }
--- pwm.h
+++ pwm.h
@@ -4,10 +4,13 @@
  * Created by:     Jawad Nasser
  * Modified by:    Adrian Padin
  * 			       Emily Rowland
- * 				   Ben Roland Miron
+ * 				   Ben Miron
  * Start date:     23 March 2016
- * Last modified:  23 March 2016
+ * Last modified:  24 March 2016
  \**************************************/
+
+#ifndef PWM_H
+#define PWM_H
 
 #include <stdio.h>
 #include <inttypes.h>
@@ -16,53 +19,38 @@
 
 #define APB_BASE_ADDR 0x40050000;
 
-#define H_BRIDGE_FORWARD  0x5
-#define H_BRIDGE_BACKWARD 0xA
-#define H_BRIDGE_OFF	  0x0
+#define H_BRIDGE_FORWARD  	0x5
+#define H_BRIDGE_BACKWARD 	0xA
+#define H_BRIDGE_OFF	  	0x0
+#define H_BRIDGE_RIGHT_MASK	0xC
+#define H_BRIDGE_LEFT_MASK	0x3
+#define PWM_RIGHT_MASK	0xFF00
+#define PWM_LEFT_MASK	0x00FF
 
 
-// Set the duty cycle to a value between 0 and 100
-void setPWMDuty(int duty) {
-	
-	// Duty should be between 0 and 100
-	assert(duty >= 0 && duty <= 100);
-	
-	// Write to offset 0
-	volatile uint32_t * addr_ptr = (uint32_t *) APB_BASE_ADDR;
-	*addr_ptr = duty;
-}
+// Set the duty cycle of the right side to a value between 0 and 100
+void setPWMDutyRight(uint32_t duty);
 
-// Get the duty cycle
-uint32_t getPWMDuty(void) {
-
-	// Read from offset 0
-	volatile uint32_t * addr_ptr = (uint32_t *) APB_BASE_ADDR;
-	return *addr_ptr;
-}
-
-// Set the H-Bridge bits
-void setHBridgeInputs(uint32_t H_in) {
-	
-	// These are invalid values and will harm the H-bridge
-	assert(!(H_in == 0x3));
-	assert(!(H_in == 0xC));
-
-	// First read value to do error checking, offset 4
-	volatile uint32_t * addr_ptr = (uint32_t *) APB_BASE_ADDR;
-	addr_ptr += 1;
-	
-	// Write to offset 4
-	// Write 0 first for safety with h-bridge
-	*addr_ptr = 0;
-	*addr_ptr = H_in;
-}
-
+//Sets the duty cycle of the left side to a value between 0 and 100
+void setPWMDutyLeft(uint32_t duty);
 
 // Get the duty cycle
-uint32_t getHBridgeInputs(void) {
+uint32_t getPWMDuty(void);
 
-	// Read from offset 4
-	volatile uint32_t * addr_ptr = (uint32_t *) APB_BASE_ADDR;
-	++addr_ptr;
-	return *addr_ptr;
-}
+// Set the H-Bridge bits for the right side
+void setHBridgeInputs(uint32_t H_in);
+
+// Get the duty cycle
+uint32_t getHBridgeInputs(void);
+
+//Calculates the duty cycle
+uint32_t calc_duty(uint8_t input);
+
+//Calculates the H Bridge Inputs for the right side of the car
+uint32_t calc_HBridgeRight(uint8_t input);
+
+//Calculates the H Bridge Inputs for the left side of the car
+uint32_t calc_HBridgeLeft(uint8_t input);
+
+
+#endif /* PWM_H */
