17e9409dd1781e47e07c0b78743e667dd27d85c5
Adrian Padin <apadin1@users.noreply.github.com>
2016-04-13 21:16:10 -0400

    Added files via upload

--- lights.ino
+++ lights.ino
@@ -3,17 +3,18 @@
   #include <avr/power.h>
 #endif
 
-#define PIN 6
+#define DATA_OUT 13
+#define ACK 10
 
-#define S1 5  //GPIO2
-#define S2 4  //GPIO1
-#define S3 3  //GPIO3
-#define S4 2  //GPIO0
-#define S5 1  //GPIO5
-#define S6 0  //GPIO6
-#define S7 7  //GPIO7
-#define S8 8  //GPIO8
-#define S9 9  //GPIO4
+#define S1 2  //GPIO0
+#define S2 3  //GPIO1
+#define S3 4  //GPIO2
+#define S4 5  //GPIO3
+#define S5 6  //GPIO4
+#define S6 7  //GPIO5
+#define S7 8  //GPIO6
+#define S8 9  //GPIO7
+
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = Arduino pin number (most are valid)
@@ -22,7 +23,7 @@
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(150, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(150, DATA_OUT, NEO_GRB + NEO_KHZ800);
 
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
 // pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
@@ -30,137 +31,81 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(150, PIN, NEO_GRB + NEO_KHZ800);
 // on a live circuit...if you must, connect GND first.
 
 void setup() {
-  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
-  #if defined (__AVR_ATtiny85__)
-    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
-  #endif
-  // End of trinket special code
-     Serial.begin(9600);
-pinMode(S1, INPUT_PULLUP);
-pinMode(S2, INPUT_PULLUP);
-pinMode(S3, INPUT_PULLUP);
-pinMode(S4, INPUT_PULLUP);
-pinMode(S5, INPUT_PULLUP);
-pinMode(S6, INPUT_PULLUP);
-pinMode(S7, INPUT_PULLUP);
-pinMode(S8, INPUT_PULLUP);
-pinMode(S9, OUTPUT);
+
+  Serial.begin(9600);
+
+  // Configure pins as outputs
+  pinMode(S1, INPUT_PULLUP);
+  pinMode(S2, INPUT_PULLUP);
+  pinMode(S3, INPUT_PULLUP);
+  pinMode(S4, INPUT_PULLUP);
+  pinMode(S5, INPUT_PULLUP);
+  pinMode(S6, INPUT_PULLUP);
+  pinMode(S7, INPUT_PULLUP);
+  pinMode(S8, INPUT_PULLUP);
+  pinMode(ACK, OUTPUT);
 
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
-
 }
 
 void loop() {
-int lastlastS1=0;
-int lastlastS2=0;
-int lastS1=0;
-int lastS2=0;
-int lastlastS3=0;
-int lastlastS4=0;
-int lastS3=0;
-int lastS4=0;
-int lastlastS5=0;
-int lastlastS6=0;
-int lastS5=0;
-int lastS6=0;
-int lastlastS7=0;
-int lastlastS8=0;
-int lastS7=0;
-int lastS8=0;
 
-//delay(5);
-  while(1){
- digitalWrite(S9, HIGH);
- //delay(1);  
- if(digitalRead(S3) && !digitalRead(S4)&& lastlastS3 && !lastlastS4 && lastS3 && !lastS4){
-    Serial.println(digitalRead(S3));
-    Serial.println(digitalRead(S4));
-   // digitalWrite(S9, LOW); don't do this for blackout
-    startShow(0);
-    
-    break;
-  } // BLACKOUT
- else if(digitalRead(S1) && !digitalRead(S2)&& lastlastS1 && !lastlastS2 && lastS1 && !lastS2){
-    Serial.println(digitalRead(S1));
-    Serial.println(digitalRead(S2));
-    digitalWrite(S9, LOW);
-    startShow(1);
-    break;
- } //B
- else if(!digitalRead(S1) && digitalRead(S2)&& !lastlastS1 && lastlastS2 && !lastS1 && lastS2){
-    Serial.println(digitalRead(S1));
-    Serial.println(digitalRead(S2));
-    digitalWrite(S9, LOW);
-    startShow(2);
-    break;
- } //Y
- else if(!digitalRead(S3) && digitalRead(S4) && !lastlastS3 && lastlastS4 && !lastS3 && lastS4){
-    Serial.println(digitalRead(S3));
-    Serial.println(digitalRead(S4));
-    digitalWrite(S9, LOW);
-    startShow(3);
-    break;
- } // show start
-  // end of reg shows
- else if(!digitalRead(S5) && digitalRead(S6)&& !lastlastS5 && lastlastS6 && !lastS5 && lastS6){
-    Serial.println(digitalRead(S5));
-    Serial.println(digitalRead(S6));
-    digitalWrite(S9, LOW);
-    startShow(4);
-    break;
- }// egg1
- else if(digitalRead(S5) && !digitalRead(S6) && lastlastS5 && !lastlastS6 && lastS5 && !lastS6){
-    Serial.println(digitalRead(S3));
-    Serial.println(digitalRead(S4));
-    digitalWrite(S9, LOW);
-    startShow(5);
-    break;
- }// egg2
- else if(!digitalRead(S7) && digitalRead(S8)&& !lastlastS7 && lastlastS8 && !lastS7 && lastS8){
-    Serial.println(digitalRead(S7));
-    Serial.println(digitalRead(S8));
-    digitalWrite(S9, LOW);
-    startShow(6);
-    break;
- }// egg3
- else if(digitalRead(S7) && !digitalRead(S8) && lastlastS7 && !lastlastS8 && lastS7 && !lastS8){
-    Serial.println(digitalRead(S7));
-    Serial.println(digitalRead(S8));
-    digitalWrite(S9, LOW);
-    startShow(7);
-    break;
- }// egg4
-lastlastS1=lastS1;
-lastlastS2=lastS2;
-lastS1=digitalRead(S1);
-lastS2=digitalRead(S2);
-lastlastS3=lastS3;
-lastlastS4=lastS4;
-lastS3=digitalRead(S3);
-lastS4=digitalRead(S4);
-lastlastS5=lastS5;
-lastlastS6=lastS6;
-lastS5=digitalRead(S5);
-lastS6=digitalRead(S6);
-lastlastS7=lastS7;
-lastlastS8=lastS8;
-lastS7=digitalRead(S7);
-lastS8=digitalRead(S8);
-  /* int sensorValue0 = analogRead(A0);
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float voltage0 = sensorValue0 * (5.0 / 1023.0);
-  // print out the value you read:
-  delay(30);
-  Serial.println(sensorValue0);
-   int sensorValue1 = analogRead(A1);
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float voltage1 = sensorValue1 * (3.3 / 1023.0);
-  // print out the value you read:
-   delay(30);
-  //Serial.println(voltage1); */
+  // Always keep ACK off when not in use
+  digitalWrite(ACK, HIGH); // ACK off
+  
+  // Removed Blackout show - it was throwing me off...
+
+  // BLUE
+  if(!digitalRead(S1)){
+    digitalWrite(ACK, LOW); // ACK on 
+    startShow(1);         
+  } //BLUE
+
+  // MAIZE
+  else if(!digitalRead(S2)){
+    digitalWrite(ACK, LOW); // ACK on 
+    startShow(2);         
+  } // MAIZE
+
+  // START GAME
+  else if(!digitalRead(S3)){
+    digitalWrite(ACK, LOW); // ACK on 
+    startShow(3);         
+  } // START GAME
+
+
+  
+  // EASTER EGG SHOWS
+  
+  // EGG 1
+  else if(!digitalRead(S4)){
+    digitalWrite(ACK, LOW); // ACK on 
+    startShow(4);         
+  } // EGG 1
+
+  // EGG 2
+  else if(!digitalRead(S5)){
+    digitalWrite(ACK, LOW); // ACK on 
+    startShow(5);         
+  } // EGG 2
+
+  // EGG 3
+  else if(!digitalRead(S6)){
+    digitalWrite(ACK, LOW); // ACK on 
+    startShow(6);         
+  } // EGG 3
+
+  // EGG 4
+  else if(!digitalRead(S7)){
+    digitalWrite(ACK, LOW); // ACK on 
+    startShow(7);         
+  } // EGG 4
 }
-}
+
+
+
+
 
 // Fill the dots one after the other with a color
 void colorWipe(uint32_t c, uint8_t wait) {
@@ -255,22 +200,22 @@ void startShow(int i) {
     case 1: colorWipe(strip.Color(0, 0, 255), 10);  // Blue
             theaterChase(strip.Color(  0,   0, 127), 30); // Blue
             colorWipe(strip.Color(0, 0, 0), 1); 
-            digitalWrite(S9, HIGH);
+            digitalWrite(ACK, HIGH);
             break;
     case 2: colorWipe(strip.Color(255, 255, 0), 10);  // Y
             theaterChase(strip.Color(  127,   127, 0), 30); // Y
             colorWipe(strip.Color(0, 0, 0), 1); 
-            digitalWrite(S9, HIGH);
+            digitalWrite(ACK, HIGH);
             break;
     case 3: colorWipe(strip.Color(255, 0, 0), 10); // Red
              colorWipe(strip.Color(255,255,0), 10); // Yellow
              theaterChase(strip.Color(0, 255, 0), 40); // Green
              colorWipe(strip.Color(0, 0, 0), 1); 
-             digitalWrite(S9, HIGH);
+             digitalWrite(ACK, HIGH);
             break;
     case 4: theaterChaseRainbow(20);
             colorWipe(strip.Color(0, 0, 0), 1); 
-            digitalWrite(S9, HIGH);
+            digitalWrite(ACK, HIGH);
             break;
     case 5: colorWipe(strip.Color(255, 0, 0), 5); // Red
             colorWipe(strip.Color(255, 127, 0), 5); // Orange
@@ -279,16 +224,16 @@ void startShow(int i) {
             colorWipe(strip.Color(0, 0, 255), 5); // Blue
             colorWipe(strip.Color(127,0,255), 5); // Violet
             colorWipe(strip.Color(0, 0, 0), 1); 
-            digitalWrite(S9, HIGH);
+            digitalWrite(ACK, HIGH);
             break;
     case 6: rainbowCycle(10);
             colorWipe(strip.Color(0, 0, 0), 1); 
-            digitalWrite(S9, HIGH);
+            digitalWrite(ACK, HIGH);
             break;
     case 7: rainbow(5);
             theaterChase(strip.Color(255,0,255), 10); //PINK
             colorWipe(strip.Color(0, 0, 0), 1); 
-            digitalWrite(S9, HIGH);
+            digitalWrite(ACK, HIGH);
             break;
   }
 }
